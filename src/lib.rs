2e9ee82da78e44759a754035b373298ec9d31c25
Max Wickham <maxwickham@startmail.com>
2024-03-01 15:54:05 -0500

    about to change tp phf map

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@ use lazy_static::lazy_static;
 use rand::Rng;
 use std::collections::HashMap;
 use std::sync::Mutex;
+use random_string::generate;
+use phf::phf_map;
 
 use css_rules::{
     assert_valid_border, assert_valid_color, assert_valid_dimensions, assert_valid_length,
@@ -12,23 +14,23 @@ use css_rules::{
 
 mod css_passing;
 
-#[doc(hidden)]
-pub struct GlobalCSS {
-    pub css_string: String,
-}
+// #[doc(hidden)]
+// pub struct GlobalCSS {
+//     pub css_string: String,
+// }
 
-impl GlobalCSS {
-    pub fn append_value(&mut self, value: String) {
-        self.css_string += &value;
-    }
-}
+// impl GlobalCSS {
+//     pub fn append_value(&mut self, value: String) {
+//         self.css_string += &value;
+//     }
+// }
 
-lazy_static! {
-    #[doc(hidden)]
-    pub static ref GLOBAL_CSS: Mutex<GlobalCSS> = Mutex::new(GlobalCSS {
-        css_string: String::new(),
-    });
-}
+// lazy_static! {
+//     #[doc(hidden)]
+//     pub static ref GLOBAL_CSS: Mutex<GlobalCSS> = Mutex::new(GlobalCSS {
+//         css_string: String::new(),
+//     });
+// }
 // const GOAL: Goal = Goal {
 //     value: String::new(),
 // };
@@ -37,7 +39,7 @@ lazy_static! {
 #[macro_export]
 macro_rules! create_object {
     ($key:literal, $hashmap:expr, $value:expr) => {{
-        $hashmap.insert($key.to_string(), $value.to_string());
+        // $hashmap.insert($keyuse const_random::const_random  ;.to_string(), $value.to_string());
     }};
 
     ($key:ident, $hashmap:expr, $value:literal) => {{
@@ -54,10 +56,15 @@ macro_rules! create_object {
 macro_rules! css_style {
     ($($key:tt : $value:expr),* $(,)?)  => {
         {
-            let mut map : HashMap<String,String> = HashMap::new();
-            $(
-                create_object!($key, map, $value);
-            )*
+            // let mut map : HashMap<String,String> = HashMap::new();
+            // $(
+            //     create_object!($key, map, $value);
+            // )*
+            const map : HashMap<String,String> = phf_map!{
+                $(
+                    create_object!($key, map, $value);
+                )*
+            };
             map
         }
     };
@@ -78,60 +85,94 @@ pub fn convert_map_to_css_string(map: HashMap<String, String>) -> String {
     css_string
 }
 
+// #[doc(hidden)]
+// pub fn generate_css_style_name() -> String {
+//     const CHARSET: &[u8] = b"abcdefghijklmnopqrstuvwxyz";
+//     const LENGTH: usize = 16;
+
+//     let mut rng = rand::thread_rng();
+//     let name: String = (0..LENGTH)
+//         .map(|_| {
+//             let idx = rng.gen_range(0..CHARSET.len());
+//             CHARSET[idx] as char
+//         })
+//         .collect();
+
+//     String::from("venus-") + &name
+// }
+
+// #[doc(hidden)]
+// const fn generate_css_style_name(length: usize) -> &'static str {
+//     let random_string: &'static str = (0..length)
+//         .map(|_| thread_rng().sample(Alphanumeric));
+
+//     // Use concat! and format! to convert the String to a &'static str at compile time
+//     &*format!("{}", concat!("\"", random_string, "\""))
+// }
+
 #[doc(hidden)]
-pub fn generate_css_style_name() -> String {
-    const CHARSET: &[u8] = b"abcdefghijklmnopqrstuvwxyz";
-    const LENGTH: usize = 16;
-
-    let mut rng = rand::thread_rng();
-    let name: String = (0..LENGTH)
-        .map(|_| {
-            let idx = rng.gen_range(0..CHARSET.len());
-            CHARSET[idx] as char
-        })
-        .collect();
-
-    String::from("venus-") + &name
+#[macro_export]
+macro_rules! generate_random_css_name {
+    ($len:expr) => {{
+        const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        const LEN: usize = $len;
+        let mut rng = rand::thread_rng();
+        let random_string: String = (0..LEN)
+            .map(|_| {
+                let idx = rng.gen_range(0..CHARSET.len());
+                CHARSET[idx] as char
+            })
+            .collect();
+        random_string
+    }};
 }
 
 #[macro_export]
 macro_rules! css_stylesheet {
     ({} => {}) => {};
 
-    ($($key:ident : $value:tt),* $(,)?)  => {
-
-            use venus_css::{css_style, create_object, css_key, GLOBAL_CSS, generate_css_style_name, convert_map_to_css_string};
+    {$($key:ident : $value:tt),* $(,)?}  => {
+        // {
+            use venus_css::{css_style, create_object, css_key, generate_random_css_name, convert_map_to_css_string};
             use std::collections::HashMap;
-
-            // #[derive(Debug)]
-            // #[allow(dead_code)]
-            // struct Stylesheet {
-            //     $(
-            //         pub $key: String,
-            //     )*
-            // }
-            // let result = Stylesheet {
-            //     $(
-            //         $key:  || -> String {
-            //             let id = generate_css_style_name();
-            //             let css_map = css_style! $value;
-            //             GLOBAL_CSS.lock().unwrap().append_value(id.clone() + ": " + &convert_map_to_css_string(css_map) + "\n");
-            //             id
-            //         }(),
-            //     )*
-            // };
-            $(static $key = || -> String {
-                    let id = generate_css_style_name();
-                    let css_map = css_style! $value;
-                    GLOBAL_CSS.lock().unwrap().append_value(id.clone() + ": " + &convert_map_to_css_string(css_map) + "\n");
+            use rand::Rng;
+            use random_string::generate;
+            const charset : &str = "abcdefghijklmnopqrstuvwxyz";
+            lazy_static!{
+                $(static ref $key : &'static str =
+                {
+                    const id : &'static str = stringify!(ident);
+                    const css_map: HashMap<String,String> = css_style! $value;
+                    // const css_string: &str = &convert_map_to_css_string(css_map);
+                    // Make compile time
+                    // GLOBAL_CSS.lock().unwrap().append_value(format!("{}: {}\n",
+                    //     id.clone(),
+                    //     &convert_map_to_css_string(css_map)
+                    // ));
+                    // css_style! $value;
                     id
-                }();)*
-            $(
-                css_style! $value;
-            )*
+                };
+                )*
+            }
+            // $(static $key : &'static str = || -> &'static str {
+            //         const id : &'static str = generate_random_css_name!(16);
+            //         // let css_map = css_style! $value;
+            //         // GLOBAL_CSS.lock().unwrap().append_value(format!("{}: {}\n",
+            //         // id.clone(),
+            //         // &convert_map_to_css_string(css_map)
+            //         // ));
+            //         id
+            //     }();
+            //     // css_style! $value;
+            // )*
+            // $(
+            //     css_style! $value;
+            // )*
+        // }
+
     };
 }
 
-pub fn css_text() -> String {
-    GLOBAL_CSS.lock().unwrap().css_string.clone()
-}
+// pub fn css_text() -> String {
+//     GLOBAL_CSS.lock().unwrap().css_string.clone()
+// }
